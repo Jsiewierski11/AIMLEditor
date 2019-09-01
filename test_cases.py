bee90b2ef43b377da6d1c886c3156323c9597de3
Jarid <jarid.siewierski@gmail.com>
2019-09-01 14:39:57 -0600

    Adding unittest for the import function

--- test_cases.py
+++ test_cases.py
@@ -49,6 +49,25 @@ def make_aiml():
     )
     return aiml
 
+def make_aiml2():
+    aiml = AIML().append(
+                    Category().append(
+                        Pattern().append("YOU CAN DO BETTER")
+                    ).append(
+                        Template().append("Ok I will try.").append(Oob().append(Robot()))
+                        )
+                ).append(
+                    Category().append(
+                        Pattern().append("WHAT DO YOU WANT TO TALK ABOUT")
+                    ).append(
+                        Template().append("I'll talk about whatever with you.")
+                        .append(
+                            Oob().append(Robot())
+                        )
+                    )
+    )
+    return aiml
+
 class TestFunctions(unittest.TestCase):
 
     def test_save_restore_aiml(self):
@@ -64,6 +83,13 @@ class TestFunctions(unittest.TestCase):
         cat.append(pattern) #Placing pattern inside category obj
         self.assertEqual(str(cat), '<category><pattern>HELLO *</pattern></category>')
 
+    def test_import_export(self):
+        expected = make_aiml2()
+        #NOTE: Make sure to not have the '.aiml' after file name. 
+        #      Causes an aborted core dump. Why?
+        imported = Storage.importAIML('/home/jarid/DFT/Test_Save/atomic')
+        self.assertEqual(str(expected), str(imported))
 
 if __name__ == '__main__':
     unittest.main()
+    # make_aiml2()
