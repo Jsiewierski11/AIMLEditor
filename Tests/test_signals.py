04a8d4f79fc7db79b960acc074116d79c6b24ff8
Jarid <jarid.siewierski@gmail.com>
2019-12-17 11:52:06 -0700

    Trying to implement example test from docs, not working and currently commented out.

--- Tests/test_signals.py
+++ Tests/test_signals.py
@@ -26,3 +26,24 @@ class TestFunctions(unittest.TestCase):
 
 if __name__ == '__main__':
     unittest.main()
+
+
+
+# HACK: Trying to implement example for the docs.
+#       Not working though, throwing error on connect
+'''
+def test_long_computation(qtbot):
+    # app = QApplication(sys.argv)
+
+    wnd = EditorWindow()
+
+    # Watch for the app.worker.finished signal, then start the worker.
+    with qtbot.waitSignal(wnd.categoryCreated, timeout=10000) as blocker:
+        blocker.connect(wnd.categoryCreated.failed)  # Can add other signals to blocker
+        wnd.categoryCreated.start()
+        # Test will block at this point until either the "finished" or the
+        # "failed" signal is emitted. If 10 seconds passed without a signal,
+        # TimeoutError will be raised.
+
+    assert_application_results(app)
+'''
